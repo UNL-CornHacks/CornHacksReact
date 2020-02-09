43ea7ad7db76669a2e3c04c7dbf333b2d57e3076
Parker Segal <parkersungwoo@gmail.com>
2020-02-09 15:52:35 -0600

    Updates

---
+++ precache-manifest.03812d7415a036f7c204d90100fdaedb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd3fd711bf5793b1ae3a4a025ae89b8",
+    "url": "/GithubPageReactTest/index.html"
+  },
+  {
+    "revision": "8cf69cbd96b8d0498eb3",
+    "url": "/GithubPageReactTest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58af7788e951a143bf88",
+    "url": "/GithubPageReactTest/static/js/2.f258fc50.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/GithubPageReactTest/static/js/2.f258fc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf69cbd96b8d0498eb3",
+    "url": "/GithubPageReactTest/static/js/main.1e5d476c.chunk.js"
+  },
+  {
+    "revision": "3364642df28d9495ebd5",
+    "url": "/GithubPageReactTest/static/js/runtime-main.2845fd77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GithubPageReactTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
