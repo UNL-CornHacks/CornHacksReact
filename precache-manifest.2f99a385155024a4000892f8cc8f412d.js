aca4f711e0512744f794f303b6fe8bb20cef1f07
Parker Segal <parkersungwoo@gmail.com>
2020-02-09 16:28:47 -0600

    Updates

---
+++ precache-manifest.2f99a385155024a4000892f8cc8f412d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8942bde4a0587f16ae7f0a565c75b8cd",
+    "url": "/GithubPageReactTest/index.html"
+  },
+  {
+    "revision": "80935551b632dee8be03",
+    "url": "/GithubPageReactTest/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "58af7788e951a143bf88",
+    "url": "/GithubPageReactTest/static/js/2.f258fc50.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/GithubPageReactTest/static/js/2.f258fc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80935551b632dee8be03",
+    "url": "/GithubPageReactTest/static/js/main.17f36b56.chunk.js"
+  },
+  {
+    "revision": "3364642df28d9495ebd5",
+    "url": "/GithubPageReactTest/static/js/runtime-main.2845fd77.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/GithubPageReactTest/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85d3adfd0e06f0c72be1a9c516c7b45e.js"
+  "/GithubPageReactTest/precache-manifest.2f99a385155024a4000892f8cc8f412d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GithubPageReactTest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
