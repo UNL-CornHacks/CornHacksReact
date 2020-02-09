389f9e52ec4cc83fee3f5096e90b3d76ad0d3a62
Parker Segal <parkersungwoo@gmail.com>
2020-02-09 17:25:05 -0600

    Testing change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CornHacks 2021
+          CornHacks 2021 Website!
         </p>
         <a
           className="App-link"
