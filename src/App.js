da574cc7c7fa46126810b9399c39672dc632966e
Peyton Tanzillo <peyton.tanzillo@huskers.unl.edu>
2020-02-16 17:32:41 -0600

    Updated website to be basic placeholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./cornhacks.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>CornHacks 2021 Website!</p>
         <p>
-          CornHacks 2021 Website!
+          Questions? Contact us at:
+          <br />
+          <a
+            className="App-link"
+            href="mailto:info@cornhacks.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            info@cornhacks.com
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
