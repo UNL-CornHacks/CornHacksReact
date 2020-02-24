2f4e6c775de621f5fee174ec1537b3ffe5c9ec91
Peyton Tanzillo <peyton.tanzillo@huskers.unl.edu>
2020-02-23 20:51:55 -0600

    Displayed sponsor names of different tiers in react

--- src/Pages/SandboxPage.js
+++ src/Pages/SandboxPage.js
@@ -1,8 +1,15 @@
 import Airtable from "airtable";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Colors from "Constants/Colors";
 
 const SandboxPage = () => {
+  const [sponsorData, setSponsorData] = useState({
+    platinum: [],
+    gold: [],
+    silver: [],
+    bronze: []
+  });
+
   useEffect(() => {
     const base = new Airtable({
       apiKey: process.env.REACT_APP_AIRTABLE_API_KEY
@@ -17,12 +24,80 @@ const SandboxPage = () => {
           console.error(err);
           return;
         }
-        records.forEach(function(record) {
-          console.log("Retrieved", record.get("Company"));
+        console.log(records);
+        setSponsorData({
+          platinum: records.filter(
+            record => record.fields["Sponsor Level"] === "Platinum"
+          ),
+          gold: records.filter(
+            record => record.fields["Sponsor Level"] === "Gold"
+          ),
+          silver: records.filter(
+            record => record.fields["Sponsor Level"] === "Silver"
+          ),
+          bronze: records.filter(
+            record => record.fields["Sponsor Level"] === "Bronze"
+          )
         });
       });
-  });
-  return <div style={pageStyling}></div>;
+  }, []);
+  console.log(sponsorData);
+  if (sponsorData) {
+    return (
+      <div style={pageStyling}>
+        <div style={pageContentStyling}>
+          <p style={paragraph}>
+            <b>Platinum Sponsors:</b>
+            <br />
+            {sponsorData.platinum.map(sponsor => (
+              <>
+                <li>{sponsor.fields["Company"]}</li>
+                <br />
+              </>
+            ))}
+            <br />
+            <b>Gold Sponsors:</b>
+            <br />
+            {sponsorData.gold.map(sponsor => (
+              <>
+                <li>{sponsor.fields["Company"]}</li>
+                <br />
+              </>
+            ))}
+            <br />
+            <b>Silver Sponsors:</b>
+            <br />
+            {sponsorData.silver.map(sponsor => (
+              <>
+                <li>{sponsor.fields["Company"]}</li>
+                <br />
+              </>
+            ))}
+            <br />
+            <b>Bronze Sponsors:</b>
+            <br />
+            <ul>
+              {sponsorData.bronze.map(sponsor => (
+                <>
+                  <li>{sponsor.fields["Company"]}</li>
+                  <br />
+                </>
+              ))}
+            </ul>
+            <br />
+          </p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div style={pageStyling}>
+        <div style={pageContentStyling}>
+          <p style={paragraph}>Loading...</p>
+        </div>
+      </div>
+    );
+  }
 };
 
 /*
@@ -35,6 +110,21 @@ const pageStyling = {
   background: `radial-gradient(${Colors.BLUE}, ${Colors.DARK_BLUE})`,
   position: "fixed",
   display: "flex"
+};
+
+const pageContentStyling = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100%",
+  textAlign: "center",
+  flexDirection: "column"
+};
+
+const paragraph = {
+  color: Colors.WHITE,
+  fontSize: "25px"
 };
 
 export default SandboxPage;
