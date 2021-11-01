59adb0d96f6dca8cf79452d47cf649abb402d4cf
cordellrhoads <rhoadscordell7@gmail.com>
2021-11-01 18:30:08 -0500

    Updating text to signify 2022 schedule coming soon

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -24,7 +24,7 @@ const HomePage = () => {
             <HomeHero showOverlay={() => setOverlayState(true)} />
             <TextSection backgroundColor={Colors.DARK_BLUE}>
                 <Type size="h2" bold align="center">
-                    Schedule is now available!
+                    2022 Schedule coming soon!
                 </Type>
                 <div className="ch-button-container-center">
                 <Button inverted internal to="/schedule/">
