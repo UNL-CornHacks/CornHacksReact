9c078a3801586e5c77bc9b272a6593ad28368090
cordellrhoads <rhoadscordell7@gmail.com>
2021-11-01 18:24:19 -0500

    Reset Countdown

--- src/Constants/ImportantDates.js
+++ src/Constants/ImportantDates.js
@@ -1,5 +1,6 @@
 const ImportantDates = {
-    START_DATE: new Date(Date.UTC(2021, 0, 30, 17)), // January 30th, 2021 at 11:00am CST
+    // START_DATE: new Date(Date.UTC(2021, 0, 30, 17)), // January 30th, 2021 at 11:00am CST
+    START_DATE: new Date(Date.UTC(2022, 0, 22, 17)), // January 22nd, 2022 at 11:00am CST
 };
 
 export default ImportantDates;
