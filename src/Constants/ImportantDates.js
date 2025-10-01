f95a49f6c93438a241ec87e6765f87beb437cc5d
erbey uribe <erbey.uribe@gmail.com>
2025-10-01 16:19:24 -0500

    fixed month on countdown

--- src/Constants/ImportantDates.js
+++ src/Constants/ImportantDates.js
@@ -1,5 +1,5 @@
 const ImportantDates = {
-  START_DATE: new Date(Date.UTC(2025, 11, 8, 10)), // November 8, 2025, 10am CST
+  START_DATE: new Date(Date.UTC(2025, 10, 8, 10)), // November 8, 2025, 10am CST
 };
 
 export default ImportantDates;
